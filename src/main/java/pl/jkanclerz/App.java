00796f6dea28ac7426ddf1b77165269f7fc2e224
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2023-03-18 16:56:35 +0100

    initial commit for my ecommerc projet

---
+++ src/main/java/pl/jkanclerz/App.java
@@ -0,0 +1,38 @@
+package pl.jkanclerz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Greeter greeter = new Greeter();
+
+        List<String> names = Arrays.asList("Agnieszka", "Michal", "Ola", "Barbara", "Aleksander");
+
+        //imperatywny
+        List<String> ladies = new ArrayList<>();
+        for (String name : names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+        for (String name : ladies) {
+            greeter.greet(name);
+        }
+
+        System.out.println(".......................................");
+
+        List<String> uppercasedLadies = names.stream()
+                .filter(name -> name.endsWith("a")) // lambda name: name[-1] == 'a'
+                .map(String::toUpperCase)
+                .toList();
+
+        //greet only ladies
+        uppercasedLadies.forEach(greeter::greet);
+    }
+}
